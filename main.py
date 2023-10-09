a007b5614f7f40e75c2c750dc827f4ad0a5c8fd0
Victor Drijkoningen <victor@maatinit.nl>
2023-10-09 23:08:23 +0200

    add working schedule

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+import time, datetime
+import os
+import schedule
 
-print("Test Execution Started")
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-ssl-errors=yes')
-options.add_argument('--ignore-certificate-errors')
-with webdriver.Remote(command_executor='http://localhost:4444/wd/hub',
-                      options=options
-                      ) as driver:
+def update_esphome(esphometarget, seleniumtarget):
+    print("Starting ESPHOME Update All")
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-ssl-errors=yes')
+    options.add_argument('--ignore-certificate-errors')
+    with webdriver.Remote(command_executor='http://'+seleniumtarget+'/wd/hub',
+                        options=options
+                        ) as driver:
 
-    #maximize the window size
-    driver.maximize_window()
-    time.sleep(0.5)
-    #navigate to browserstack.com
-    driver.get("http://192.168.2.115:6052/")
-    time.sleep(1)
+        #maximize the window size
+        driver.maximize_window()
+        time.sleep(0.5)
 
-    try:
-        sroot = driver.find_element(By.XPATH, "//header/esphome-header-menu").shadow_root
-        print("found shadowroot")
-        sroot.find_element(By.CSS_SELECTOR, "mwc-button").click()
-        time.sleep(5)
-        
-        esphomeconfdia = driver.find_element(By.XPATH, "//esphome-confirmation-dialog").shadow_root
-        dialog = esphomeconfdia.find_element(By.CSS_SELECTOR, "mwc-dialog")
-        dialog.find_element(By.XPATH, "mwc-button[2]").click()
+        driver.get('http://'+esphometarget)
+        time.sleep(1)
 
-    except Exception as e:
-        print(e)
-    time.sleep(10)
+        try:
+            #press first update_all button
+            driver.find_element(By.XPATH, "//esphome-header-menu").shadow_root.find_element(By.CSS_SELECTOR, "mwc-button").click()
+            time.sleep(5)
+            #press second update_all button in dialog
+            driver.find_element(By.XPATH, "//esphome-confirmation-dialog").shadow_root.find_element(By.CSS_SELECTOR, "mwc-dialog").find_element(By.XPATH, "mwc-button[2]").click()
 
+        except Exception as e:
+            print(e)
+        time.sleep(1000)
+
+def job():
+    if datetime.date.today().day not in [9]:
+        print("Not today m'dude", datetime.date.today().day)
+        return
+    #check environ:
+    #TODO
+
+    update_esphome(os.environ['ESPHOME_TARGET'], os.environ['SELENIUM_TARGET'])
+
+if __name__ == "__main__":
+    schedule.every().day.at("01:00").do(job)
+    print("Schedule Started")
+
+    #TODO check esp en selenium target
+
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
     
-
- 
-print("Test Execution Successfully Completed!")
