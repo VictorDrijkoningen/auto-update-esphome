174e265d41692cad2ca767a659b33b6490c183b6
Victor Drijkoningen <victor@maatinit.nl>
2023-10-11 21:49:11 +0200

    chnage to better naming conf

--- start esphomeupdater.sh
+++ start esphomeupdater.sh
@@ -1,9 +1,9 @@
-sudo docker build ./ --tag=esphomeupdate
+sudo docker build ./ --tag=auto-update-esphome
 
-sudo docker rm -f ESPHOME_UPDATER
+sudo docker rm -f auto-update-esphome
 
 sudo docker run -d \
-  --name="ESPHOME_UPDATER" \
+  --name="auto-update-esphome" \
   -e ESPHOME_TARGET="192.168.2.115:6052" \
-  -e SELENIUM_TARGET="172.17.0.3:4444" esphomeupdate
+  -e SELENIUM_TARGET="172.17.0.3:4444" auto-update-esphome
 
