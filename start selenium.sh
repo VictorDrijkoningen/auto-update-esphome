7198aaf390e51a470e91e52708152426f7746408
Victor Drijkoningen <victor@maatinit.nl>
2023-10-11 22:36:42 +0200

    add removal of old container and name

--- start selenium.sh
+++ start selenium.sh
@@ -3,4 +3,8 @@ sudo docker pull selenium/standalone-chrome:latest
 
 #sudo docker run -d -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome
 
-sudo docker run -d -p 4444:4444 selenium/standalone-chrome
+sudo docker rm -f Selenium
+
+sudo docker run -d -p 4444:4444 \
+  --name="Selenium" \
+  selenium/standalone-chrome
