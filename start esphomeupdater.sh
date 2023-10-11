7b57bfc6ed1ca21a5537ee7622855716d758cbac
Victor Drijkoningen <victor@maatinit.nl>
2023-10-11 22:36:18 +0200

    change script to correct tags

--- start esphomeupdater.sh
+++ start esphomeupdater.sh
@@ -1,9 +1,9 @@
-sudo docker build ./ --tag=auto-update-esphome
+sudo docker build ./ --tag=victordrijkoningen/auto-update-esphome
 
 sudo docker rm -f auto-update-esphome
 
 sudo docker run -d \
   --name="auto-update-esphome" \
   -e ESPHOME_TARGET="192.168.2.115:6052" \
-  -e SELENIUM_TARGET="172.17.0.3:4444" auto-update-esphome
+  -e SELENIUM_TARGET="172.17.0.3:4444" victordrijkoningen/auto-update-esphome
 
