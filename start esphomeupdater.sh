52596ac6b51620321eb943d17143ac1104b3da37
Victor Drijkoningen <victor@maatinit.nl>
2023-10-19 21:29:50 +0200

    update dev script to be split up

--- start esphomeupdater.sh
+++ start esphomeupdater.sh
@@ -1,12 +1,10 @@
 echo "WANRING this is a dev script!"
 
-sudo docker build ./ --tag=victordrijkoningen/auto-update-esphome:latest
-
 sudo docker rm -f auto-update-esphome
 
 sudo docker run -d \
   --name="auto-update-esphome" \
-  -e ESPHOME_TARGET="192.168.2.115:6052" \
+  -e ESPHOME_TARGET="IP:PORT" \
   -e MODE="selenium" \
   -v /dev/shm:/dev/shm \
   victordrijkoningen/auto-update-esphome
