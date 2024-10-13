80a79183614473af040b7edfee97597696cea71c
Victor Drijkoningen <>
2024-10-13 17:21:10 +0200

    mount dev screenshots

--- dev-scripts/start_esphomeupdater.sh
+++ dev-scripts/start_esphomeupdater.sh
@@ -4,5 +4,6 @@ sudo docker rm -f auto-update-esphome
 
 sudo docker run -d \
   --name="auto-update-esphome" \
+  -v /tmp/esphomedev:/tmp/screenshots \
   --env-file .env \
   victordrijkoningen/auto-update-esphome
