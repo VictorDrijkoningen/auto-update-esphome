c3d9cf302cd595a2719dd7f8417048c65c9feca3
Victor Drijkoningen <>
2024-11-12 21:22:10 +0100

    add script to upload archive images

---
+++ dev-scripts/push_with_tag_esphome.sh
@@ -0,0 +1,7 @@
+echo "WARNING this is a dev script!"
+
+sudo docker buildx create --name multibuilder
+
+sudo docker buildx use multibuilder
+
+sudo docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t victordrijkoningen/auto-update-esphome:$(cat ./VERSION) --push .
