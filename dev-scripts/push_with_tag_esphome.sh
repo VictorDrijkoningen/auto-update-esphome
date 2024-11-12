f66bf7371c4012fbf2247976ecf041f6eb11332a
Victor Drijkoningen <>
2024-11-12 22:15:03 +0100

    split builders

--- dev-scripts/push_with_tag_esphome.sh
+++ dev-scripts/push_with_tag_esphome.sh
@@ -1,7 +1,7 @@
 echo "WARNING this is a dev script!"
 
-sudo docker buildx create --name multibuilder
+sudo docker buildx create --name multibuilder_with_tagname
 
-sudo docker buildx use multibuilder
+sudo docker buildx use multibuilder_with_tagname
 
-sudo docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t victordrijkoningen/auto-update-esphome:$(cat ./VERSION) --push .
+sudo docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t "victordrijkoningen/auto-update-esphome:$(cat ./VERSION)" --push .
