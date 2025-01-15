5cddc64fef195a9fd145ff4382c0c0892dded7d6
Victor Drijkoningen <>
2025-01-15 17:19:58 +0100

    move helper functions to helpers.py

---
+++ helpers.py
@@ -0,0 +1,164 @@
+import os
+import datetime
+import platform
+import re
+
+def save_screenshot(config_dir, driver, tag: str) -> None:
+    '''if the development env var is set, then store the screenshot'''
+    if os.environ.get("SCREENSHOT_LOG") == "TRUE":
+        driver.save_screenshot(f"{config_dir}screenshots/{datetime.date.today()}-{tag}.png")
+
+
+def check_config_dir(config_dir: str) -> None:
+    '''checks or creates the config dir'''
+    if not os.path.isdir(config_dir):
+        os.mkdir(config_dir)
+
+
+def trim_log(log_file: str) -> None:
+    '''trim log file to 250 on startup'''
+    if log_size(log_file) > 250:
+        try:
+            with open(log_file, 'r', encoding="utf-8") as scr, open(log_file+"-"+str(datetime.date.today())+".log", 'w', encoding="utf-8") as dst:
+                for line in scr:
+                    dst.write(line)
+            with open(log_file, "w", encoding="utf-8") as logf:
+                d = datetime.date.today()
+                logf.write(f"logs split on {d}")
+        except Exception as e:
+            log(log_file, "LOG TRIM ERROR" + str(e))
+
+
+def log(log_file: str, message: str, timestamp=True) -> None:
+    '''global logging function'''
+    if timestamp:
+        d = datetime.date.today()
+        inp = f'{d} - {message}'
+    else:
+        inp = f'{message}.'
+
+    print(inp)
+    with open(log_file, "a", encoding="utf-8") as logf:
+        logf.write(inp+"\n")
+
+
+def log_size(log_file: str) -> int:
+    '''get log line count'''
+    with open(log_file, "r", encoding="utf-8") as logf:
+        count = 0
+        for count, _ in enumerate(logf):
+            pass
+        return count
+
+
+def check_geckodriver(log_file: str, driver_dir: str, driver_tar: str, driver_link: str, driver_version: str) -> None:
+    '''check geckodriver status and download / update if applicable'''
+    if platform.machine() == "aarch64":
+        print("arm64 detected! Checking Gecko driver status")
+        if not os.path.isdir(driver_dir):
+            log(log_file, "Gecko driver directory not found, making it...")
+            os.mkdir(driver_dir)
+
+            import requests
+            log(log_file, "Downloading driver...")
+            try:
+                response = requests.get(driver_link, timeout=60)
+            except requests.exceptions.Timeout:
+                log(log_file, "Could not download driver, timed out, exiting...")
+                exit(1)
+
+            response.raise_for_status()
+
+            with open(driver_tar, 'wb') as file:
+                file.write(response.content)
+
+            log(log_file, "Extracting driver...")
+            import tarfile
+            file = tarfile.open(driver_tar)
+            file.extractall(driver_dir, filter='data')
+            file.close()
+
+            with open(driver_dir+"version", "w", encoding="utf-8") as file:
+                file.write(driver_version)
+
+            log(log_file, "Driver seems successfully downloaded")
+        else:
+            if os.path.exists(driver_dir+"geckodriver"):
+                print("Found driver")
+            else:
+                log(log_file, "ERROR: found empty driver directory")
+                exit(1)
+
+
+def check_env(log_file):
+    '''check the environment variables to be suitable for this code'''
+    ip_regex = r'[0-9]+(?:\.[0-9]+){3}:[0-9]+' #should not exclude ipv6, but good enough for now.
+
+    #check MODE
+    if os.environ.get('MODE') != 'selenium' and os.environ.get('MODE') != 'socket':
+        log(log_file, f"ERROR: unknown mode {os.environ.get('MODE')}")
+        exit(1)
+
+    #check IP esphome
+    if not re.search(ip_regex, str(os.environ.get('ESPHOME_TARGET'))):
+        log(log_file, "ERROR: esphome target not valid")
+        exit(1)
+
+    #check auth
+    if not os.environ.get('PASSWORD') is None:
+        if os.environ.get('USERNAME') is None:
+            log(log_file, "ERROR: USERNAME EMPTY")
+            exit(1)
+    if not os.environ.get('USERNAME') is None:
+        if os.environ.get('PASSWORD') is None:
+            log(log_file, "ERROR: PASSWORD EMPTY")
+            exit(1)
+
+    if os.environ.get('USERNAME') is None and os.environ.get('PASSWORD') is None:
+        log(log_file, "No credentials found, assuming no credentials needed")
+    else:
+        log(log_file, "Credentials found, rolling with credentials")
+
+    #check logging
+    if os.environ.get("SCREENSHOT_LOG") == "TRUE":
+        log(log_file, "Logging screenshots")
+
+    #check RUN_MONTHS
+    if os.environ.get('RUN_MONTHS') is None:
+        log(log_file, "No months to run found, setting to all months")
+        os.environ.setdefault('RUN_MONTHS', '1,2,3,4,5,6,7,8,9,10,11,12')
+
+    run_months = os.environ.get('RUN_MONTHS')
+    run_months = run_months.replace(' ', '').split(',')
+    if len(run_months) == 0:
+        log(log_file, "ERROR: RUN_MONTHS env found no months")
+        exit(1)
+    try:
+        run_months = [int(i) for i in run_months]
+    except ValueError:
+        log(log_file, "ERROR: faulty value in RUN_MONTHS")
+        exit(1)
+
+    #check days
+    if os.environ.get('RUN_DAYS') is None:
+        log(log_file, "No days to run found, setting to all days")
+        os.environ.setdefault('RUN_DAYS', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31')
+
+    run_days = os.environ.get('RUN_DAYS')
+    run_days = run_days.replace(' ', '').split(',')
+    if len(run_days) == 0:
+        log(log_file, "ERROR: RUN_DAYS env found no days")
+        exit(1)
+    try:
+        run_days = [int(i) for i in run_days]
+    except ValueError:
+        log(log_file, "ERROR: faulty value in RUN_DAYS")
+        exit(1)
+
+    #check time
+    if os.environ.get("RUN_TIME") is None:
+        log(log_file, "ERROR: No RUN_TIME detected")
+        exit(1)
+    if not len(os.environ.get("RUN_TIME")) == 5:
+        log(log_file, "ERROR: RUN_TIME in wrong format")
+        exit(1)
--- main.py
+++ main.py
@@ -2,70 +2,25 @@ import os
 import platform
 import time
 import datetime
-import re
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 from selenium.webdriver.common.by import By
 import selenium.common.exceptions
 import schedule
+from helpers import check_config_dir, trim_log, log, check_geckodriver, check_env, save_screenshot
 
-CONFIGDIR = "/config"
+CONFIGDIR = "/config/"
 DRIVERTAR = "/config/driver/download.tar.gz"
 DRIVERDIR = "/config/driver/"
 LINKAARCH64DRIVER = "https://github.com/mozilla/geckodriver/releases/download/v0.35.0/geckodriver-v0.35.0-linux-aarch64.tar.gz"
+DRIVERVERSION="0.35.0"
 LOGFILE = "/config/app.log"
-
-def check_or_create_config_dir():
-    '''does what the name implies'''
-    if not os.path.isdir(CONFIGDIR):
-        os.mkdir(CONFIGDIR)
-
-
-def save_screenshot(driver, tag: str) -> None:
-    '''if the development env var is set, then store the screenshot'''
-    if os.environ.get("SCREENSHOT_LOG") == "TRUE":
-        driver.save_screenshot(f"{CONFIGDIR}/screenshots/{datetime.date.today()}-{tag}.png")
-
-
-def log_size():
-    '''get log line count'''
-    with open(LOGFILE, "r", encoding="utf-8") as logf:
-        for count, _ in enumerate(logf):
-            pass
-        return count
-
-  
-def trim_log():
-    '''trim log file to 250 on startup'''
-    if log_size() > 250:
-        try:
-            with open(LOGFILE, 'r') as scr, open(LOGFILE+"-"+str(datetime.date.today())+".log", 'w') as dst:
-                for line in scr:
-                    dst.write(line)
-            with open(LOGFILE, "w", encoding="utf-8") as logf:
-                d = datetime.date.today()
-                logf.write(f"logs split on {d}")
-        except Exception as e:
-            log("LOG TRIM ERROR" + str(e))
-
-
-def log(message: str, timestamp=True) -> None:
-    '''global logging function'''
-    if timestamp:
-        d = datetime.date.today()
-        inp = f'{d} - {message}.'
-    else:
-        inp = f'{message}.'
-    
-    print(inp)
-    with open(LOGFILE, "a", encoding="utf-8") as logf:
-        logf.write(inp+"\n")
 
 
 def update_esphome_via_selenium(driver, esphometarget, authentication = None):
     '''update esphome devices via a selenium operated firefox instance'''
 
-    log("Starting ESPHOME Update All")
+    log(LOGFILE, "Starting ESPHOME Update All")
 
     driver.maximize_window()
     driver.get('http://'+esphometarget)
@@ -74,28 +29,28 @@ def update_esphome_via_selenium(driver, esphometarget, authentication = None):
     try:
         if not (authentication is None or authentication == [None, None] ):
             #There is authentication needed
-            save_screenshot(driver, "1.beforeauth")
+            save_screenshot(CONFIGDIR, driver, "1.beforeauth")
             el = "username-field"
             input_username = driver.find_element(By.ID , el)
             input_username.send_keys(authentication[0])
             el = "password-field"
             input_username = driver.find_element(By.ID , el)
             input_username.send_keys(authentication[1])
-            save_screenshot(driver, "2.aftertyping")
+            save_screenshot(CONFIGDIR, driver, "2.aftertyping")
             el = "login-form-submit"
             input_submit = driver.find_element(By.ID, el)
             input_submit.click()
-            save_screenshot(driver, "3.afterauth")
+            save_screenshot(CONFIGDIR, driver, "3.afterauth")
 
         time.sleep(10) #wait for page-load
 
         try:
             el = "login-form-submit"
             ans_loggedin = driver.find_element(By.ID, el)
-            log("ERROR: LOGIN FAILED")
+            log(LOGFILE, f"ERROR: LOGIN FAILED; {ans_loggedin}")
             return 2
         except selenium.common.exceptions.NoSuchElementException:
-            log("Successfully logged in")
+            log(LOGFILE, "Successfully logged in")
 
         #check if devices are up-to-date
         el = "//esphome-devices-list"
@@ -113,10 +68,10 @@ def update_esphome_via_selenium(driver, esphometarget, authentication = None):
                 updateable_devices += 1
 
         if not found_updateable:
-            log("no updates found for devices, done updating")
+            log(LOGFILE, "no updates found for devices, done updating")
             return 1
 
-        log(f"Found {updateable_devices} devices that can be updated")
+        log(LOGFILE, f"Found {updateable_devices} devices that can be updated")
 
 
         #press first update_all button
@@ -124,7 +79,7 @@ def update_esphome_via_selenium(driver, esphometarget, authentication = None):
         button_encasing = driver.find_element(By.XPATH, el).shadow_root
         el = "mwc-button"
         button_encasing.find_element(By.CSS_SELECTOR, el).click()
-        save_screenshot(driver, "4.dialog")
+        save_screenshot(CONFIGDIR, driver, "4.dialog")
 
 
         time.sleep(2)
@@ -136,7 +91,7 @@ def update_esphome_via_selenium(driver, esphometarget, authentication = None):
         button_encasing.find_element(By.XPATH, "mwc-button[2]").click()
 
         #wait for all esp devices to be updated
-        log("waiting for update to finish")
+        log(LOGFILE, "waiting for update to finish")
 
         #wait for summary to appear or timeout this action
         starttime = time.time()
@@ -144,28 +99,28 @@ def update_esphome_via_selenium(driver, esphometarget, authentication = None):
         while True:
             time.sleep(10)
             if time.time() - starttime > 1000:
-                log("ERROR: Failed to find update dialog, update failed!")
-                save_screenshot(driver, "5.failed")
+                log(LOGFILE, "ERROR: Failed to find update dialog, update failed!")
+                save_screenshot(CONFIGDIR, driver, "5.failed")
                 break
 
             #compare page to see if nothing is changing anymore
             newpage = driver.get_screenshot_as_base64()
             if newpage == oldpage:
-                log(f"end of update detected, took {round(time.time()-starttime)} seconds")
-                save_screenshot(driver, "5.success")
+                log(LOGFILE, f"end of update detected, took {round(time.time()-starttime)} seconds")
+                save_screenshot(CONFIGDIR, driver, "5.success")
                 break
             oldpage = newpage
 
     except selenium.common.exceptions.NoSuchElementException as e:
-        log("Some elements could not be found in esphome", e)
-        log(f"element: {el}")
-        log("ERROR: ESPHOME UPDATING FAILED")
+        log(LOGFILE, f"Some elements could not be found in esphome {e}")
+        log(LOGFILE, f"element: {el}")
+        log(LOGFILE, "ERROR: ESPHOME UPDATING FAILED")
     return 0
 
 
 def update_esphome_via_socket(esphometarget, auth):
     '''Update esphome devices via a socket call'''
-    log(f"TODO socket call to {esphometarget}")
+    log(LOGFILE, "TODO socket call implementation")
     exit(1)
 
 
@@ -209,126 +164,23 @@ def check_date():
     start_update()
 
 
-def check_env():
-    '''check the environment variables to be suitable for this code'''
-
-    if platform.machine() == "aarch64":
-        print("arm64 detected! Checking Gecko driver status")
-        if not os.path.isdir(DRIVERDIR):
-            log("Gecko driver directory not found, making it...")
-            os.mkdir(DRIVERDIR)
-
-            import requests
-            log("Downloading driver...")
-            response = requests.get(LINKAARCH64DRIVER)
-            response.raise_for_status()
-
-            with open(DRIVERTAR, 'wb') as file:
-                file.write(response.content)
-
-            log("Extracting driver...")
-            import tarfile
-            file = tarfile.open(DRIVERTAR)
-            file.extractall(DRIVERDIR, filter='data')
-            file.close()
-            log("Driver seems successfully downloaded")
-        else:
-            if os.path.exists(DRIVERDIR+"geckodriver"):
-                print("Found driver")
-            else:
-                log("ERROR: found empty driver directory")
-                exit(1)
-            
-
-    ip_regex = r'[0-9]+(?:\.[0-9]+){3}:[0-9]+' #should not exclude ipv6, but good enough for now.
-
-    #check MODE
-    if os.environ.get('MODE') != 'selenium' and os.environ.get('MODE') != 'socket':
-        log(f"ERROR: unknown mode {os.environ.get('MODE')}")
-        exit(1)
-
-    #check IP esphome
-    if not re.search(ip_regex, str(os.environ.get('ESPHOME_TARGET'))):
-        log("ERROR: esphome target not valid")
-        exit(1)
-
-    #check auth
-    if not os.environ.get('PASSWORD') is None:
-        if os.environ.get('USERNAME') is None:
-            log("ERROR: USERNAME EMPTY")
-            exit(1)
-    if not os.environ.get('USERNAME') is None:
-        if os.environ.get('PASSWORD') is None:
-            log("ERROR: PASSWORD EMPTY")
-            exit(1)
-
-    if os.environ.get('USERNAME') is None and os.environ.get('PASSWORD') is None:
-        log("No credentials found, assuming no credentials needed")
-    else:
-        log("Credentials found, rolling with credentials")
-
-    #check logging
-    if os.environ.get("SCREENSHOT_LOG") == "TRUE":
-        log("Logging screenshots")
-
-    #check RUN_MONTHS
-    if os.environ.get('RUN_MONTHS') is None:
-        log("No months to run found, setting to all months")
-        os.environ.setdefault('RUN_MONTHS', '1,2,3,4,5,6,7,8,9,10,11,12')
-
-    run_months = os.environ.get('RUN_MONTHS')
-    run_months = run_months.replace(' ', '').split(',')
-    if len(run_months) == 0:
-        log("ERROR: RUN_MONTHS env found no months")
-        exit(1)
-    try:
-        run_months = [int(i) for i in run_months]
-    except ValueError:
-        log("ERROR: faulty value in RUN_MONTHS")
-        exit(1)
-
-    #check days
-    if os.environ.get('RUN_DAYS') is None:
-        log("No days to run found, setting to all days")
-        os.environ.setdefault('RUN_DAYS', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31')
-
-    run_days = os.environ.get('RUN_DAYS')
-    run_days = run_days.replace(' ', '').split(',')
-    if len(run_days) == 0:
-        log("ERROR: RUN_DAYS env found no days")
-        exit(1)
-    try:
-        run_days = [int(i) for i in run_days]
-    except ValueError:
-        log("ERROR: faulty value in RUN_DAYS")
-        exit(1)
-
-    #check time
-    if os.environ.get("RUN_TIME") is None:
-        log("ERROR: No RUN_TIME detected")
-        exit(1)
-    if not len(os.environ.get("RUN_TIME")) == 5:
-        log("ERROR: RUN_TIME in wrong format")
-        exit(1)
-
-
 if __name__ == "__main__":
     print("Auto update esphome! \nQuestions? https://github.com/VictorDrijkoningen/auto-update-esphome")
-    check_or_create_config_dir()
+    check_config_dir(CONFIGDIR)
+    trim_log(LOGFILE)
+
     with open('VERSION', encoding="utf-8") as f:
-        log("\n", timestamp=False)
-        log(f"VERSION: {f.read()}")
+        log(LOGFILE, "", timestamp=False)
+        log(LOGFILE, f"VERSION: {f.read()}")
 
-    trim_log()
-
-    #check environment variables
-    check_env()
+    check_env(LOGFILE)
+    check_geckodriver(LOGFILE, DRIVERDIR, DRIVERTAR, LINKAARCH64DRIVER, DRIVERVERSION)
 
     if os.environ.get('UPDATE_ON_STARTUP') == 'TRUE':
         start_update()
 
     schedule.every().day.at(os.environ.get("RUN_TIME")).do(check_date)
-    log("Schedule Started")
+    log(LOGFILE, "Schedule Started")
     while True:
         schedule.run_pending()
-        time.sleep(10)
+        time.sleep(30)
