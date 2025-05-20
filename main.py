7de5cba903f7d4d736960ae64ac7623873284582
VictorDrijkoningen <72612250+VictorDrijkoningen@users.noreply.github.com>
2025-05-20 12:27:25 +0200

    add other test

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def update_esphome_via_selenium(driver, esphometarget, authentication = None):
 
         if not found_updateable:
             log(LOGFILE, "no updates found for devices, done updating")
-            return 1
+            # return 1
 
         log(LOGFILE, f"Found {updateable_devices} devices that can be updated")
 
@@ -102,7 +102,7 @@ def update_esphome_via_selenium(driver, esphometarget, authentication = None):
             time.sleep(30)
             screenshotcount += 1
             save_screenshot(CONFIGDIR, driver, f"4.{screenshotcount}updateprocess")
-            
+
             if time.time() - starttime > 1000:
                 log(LOGFILE, "ERROR: Failed to find update dialog, update failed!")
                 save_screenshot(CONFIGDIR, driver, "5.failed")
